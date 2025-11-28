47a69d6e9c84c67f635851f874fa9fd2e9a65cbb
Tay Seok Eng <monique.tay@gmail.com>
2025-11-28 12:48:15 +0800

    Create q7.py

---
+++ q7.py
@@ -0,0 +1,54 @@
+# q7.py
+
+class Car:
+    """
+    Task 1
+    - Define a class named Car with attributes: make, model, year
+    - Initialize these attributes in the __init__ method
+    - Add a method named describe_car() that prints information about the car as "Year Make Model"
+    """
+
+    def describe_car(self):
+        print("")
+
+
+# Task 2
+# Create an instance of the Car class with the following attributes and call describe_car method:
+# - Make: Toyota, Model: Corolla, Year: 2020
+
+# a7.py
+
+class Car:
+     
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def describe_car(self):
+        print(f"{self.year} {self.make} {self.model}")
+
+
+# Task 2
+# Create an instance of the Car class and call describe_car method:
+car1 = Car("Toyota", "Corolla", 2020)
+car1.describe_car()
+
+# Output:
+# 2020 Toyota Corolla
+ 
+# Note:
+
+# car1 = Car("Toyota", "Corolla", 2020)
+# then Python calls __init__ inside the class and fills in the values for make, model and year.
+
+# self refers to the specific object being created or used.
+
+# Whenever you access attributes in a class, you use self to show you are talking about the values belonging to that particular object.
+
+# Example inside the class:
+
+# self.make = make
+# self.model = model
+# self.year = year
+
